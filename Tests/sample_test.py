7e8737c0f31e4bd0aefcdac46b228a6631bfcb63
VigneshDhakshnamoorthy <vigneshdhakshnamoorthy@gmail.com>
2024-02-12 21:22:45 +0530

    sample

--- Tests/sample_test.py
+++ Tests/sample_test.py
@@ -5,26 +5,26 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def test_sample():
-    s = smtplib.SMTP(host='outlook.office365.com', port=587)
-    s.starttls()
-    s.login("vignesh.****@***.com", "****")
-    msg = MIMEMultipart()
-    try:
-        with open("../Report/report.html", 'r') as f:
-            attach = f.read()
-    except FileNotFoundError:
-        with open(f"{os.getcwd()}/Report/report.html", 'r') as f:
-            attach = f.read()
+# def test_sample():
+#     s = smtplib.SMTP(host='outlook.office365.com', port=587)
+#     s.starttls()
+#     s.login("vignesh.****@***.com", "****")
+#     msg = MIMEMultipart()
+#     try:
+#         with open("../Report/report.html", 'r') as f:
+#             attach = f.read()
+#     except FileNotFoundError:
+#         with open(f"{os.getcwd()}/Report/report.html", 'r') as f:
+#             attach = f.read()
 
-    message = "Hi Test Message"
-    print(message)
-    msg['From'] = "vignesh.****@***.com"
-    msg['To'] = "vignesh.****@***.com"
-    msg['Subject'] = "This is TEST"
-    msg.attach(MIMEText(message, 'plain'))
-    msg.attach(MIMEText(attach, 'html'))
+#     message = "Hi Test Message"
+#     print(message)
+#     msg['From'] = "vignesh.****@***.com"
+#     msg['To'] = "vignesh.****@***.com"
+#     msg['Subject'] = "This is TEST"
+#     msg.attach(MIMEText(message, 'plain'))
+#     msg.attach(MIMEText(attach, 'html'))
 
-    s.send_message(msg)
-    del msg
-    s.quit()
+#     s.send_message(msg)
+#     del msg
+#     s.quit()
